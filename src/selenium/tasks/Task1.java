4c96468884d384df72560343906e7b4f002810ba
Chrisalisa <kikistaurins@gmail.com>
2020-10-28 19:41:18 +0200

    all mandatory tasks complete

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,8 +3,13 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class Task1 {
     WebDriver driver;
@@ -27,12 +32,25 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+        WebElement numberBox=driver.findElement(By.id("numb"));
+        numberBox.sendKeys("trollis");
+        WebElement submit=driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button"));
+        submit.click();
+        WebElement errorMessage=driver.findElement(By.id("ch1_error"));
+        assertEquals("Please enter a number", errorMessage.getText());
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+//        Note: 49 was calculated, a bug in the website
+        WebElement numberBox=driver.findElement(By.id("numb"));
+        numberBox.sendKeys("48"); //Note: 49 was calculated, a bug in the website
+        WebElement submit=driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button"));
+        submit.click();
+        WebElement errorMessage=driver.findElement(By.id("ch1_error"));
+        assertEquals("Number is too small", errorMessage.getText());
     }
 
     @Test
@@ -41,6 +59,12 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        WebElement numberBox=driver.findElement(By.id("numb"));
+        numberBox.sendKeys("101");
+        WebElement submit=driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button"));
+        submit.click();
+        WebElement errorMessage=driver.findElement(By.id("ch1_error"));
+        assertEquals("Number is too big", errorMessage.getText());
     }
 
     @Test
@@ -48,6 +72,14 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+        WebElement numberBox=driver.findElement(By.id("numb"));
+        numberBox.sendKeys("64");
+        WebElement submit=driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button"));
+        submit.click();
+        Alert firstAlert=driver.switchTo().alert();
+        String output= firstAlert.getText();
+        assertEquals("Square root of 64 is 8.00", output);
+
     }
 
     @Test
@@ -55,5 +87,12 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
+        WebElement numberBox=driver.findElement(By.id("numb"));
+        numberBox.sendKeys("65");
+        WebElement submit=driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/button"));
+        submit.click();
+        Alert firstAlert=driver.switchTo().alert();
+        String output= firstAlert.getText();
+        assertEquals("Square root of 65 is 8.06", output);
     }
 }
--- src/selenium/tasks/Task2.java
+++ src/selenium/tasks/Task2.java
@@ -3,8 +3,14 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static junit.framework.TestCase.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class Task2 {
     WebDriver driver;
@@ -14,7 +20,7 @@ public class Task2 {
         String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://kristinek.github.io/sitetasks/provide_feedback");
+        driver.get("https://kristinek.github.io/site/tasks/provide_feedback");
     }
 
     @After
@@ -25,10 +31,25 @@ public class Task2 {
     @Test
     public void initialFeedbackPage() throws Exception {
 //         TODO:
-//         check that all field are empty and no tick are clicked
-//         "Don't know" is selected in "Genre"
+//         check that all field are empty and no tick are clicked x
+//         "Don't know" is selected in "Genre" x
 //         "Choose your option" in "How do you like us?"
 //         check that the button send is blue with white letters
+        WebElement nameField= driver.findElement(By.id("fb_name"));
+        WebElement ageField= driver.findElement(By.id("fb_age"));
+        List<WebElement> checkBoxes=driver.findElements(By.className("w3-check"));
+        List<WebElement> radioCircles=driver.findElements(By.className("w3-radio"));
+        Select opinionField= new Select(driver.findElement(By.id("like_us")));
+        WebElement submit=driver.findElement(By.cssSelector(".w3-btn-block"));
+        assertEquals("", nameField.getAttribute("value"));
+        assertEquals("", ageField.getAttribute("value"));
+        for(WebElement checkbox:checkBoxes){
+            assertFalse(checkbox.isSelected());
+        }
+        assertTrue(radioCircles.get(2).isSelected());
+        assertEquals("Choose your option", opinionField.getFirstSelectedOption().getText());
+        assertEquals("rgba(33, 150, 243, 1)", submit.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)", submit.getCssValue("color"));
     }
 
     @Test
@@ -38,6 +59,27 @@ public class Task2 {
 //         check fields are empty or null
 //         check button colors
 //         (green with white letter and red with white letters)
+        WebElement submit=driver.findElement(By.cssSelector(".w3-btn-block"));
+        submit.click();
+        Thread.sleep(1000);
+        WebElement nameField= driver.findElement(By.id("name"));
+        WebElement ageField= driver.findElement(By.id("age"));
+        WebElement languageField= driver.findElement(By.id("language"));
+        WebElement genderField= driver.findElement(By.id("gender"));
+        WebElement optionField= driver.findElement(By.id("option"));
+        WebElement commentField= driver.findElement(By.id("comment"));
+        assertEquals("",nameField.getText());
+        assertEquals("",ageField.getText());
+        assertEquals("",languageField.getText());
+        assertEquals("null",genderField.getText());
+        assertEquals("null",optionField.getText());
+        assertEquals("",commentField.getText());
+        WebElement yesButton=driver.findElement(By.cssSelector("button.w3-btn:nth-child(1)"));
+        WebElement noButton=driver.findElement(By.cssSelector("button.w3-btn:nth-child(2)"));
+        assertTrue("rgba(244, 67, 54, 1)".equals(noButton.getCssValue("background-color"))&&"rgba(255, 255, 255, 1)".equals(noButton.getCssValue("color")));
+        assertTrue("rgba(76, 175, 80, 1)".equals(yesButton.getCssValue("background-color"))&&"rgba(255, 255, 255, 1)".equals(yesButton.getCssValue("color")));
+
+
     }
 
     @Test
@@ -47,6 +89,35 @@ public class Task2 {
 //         check fields are filled correctly
 //         check button colors
 //         (green with white letter and red with white letters)
+        WebElement nameField= driver.findElement(By.id("fb_name"));
+        WebElement ageField= driver.findElement(By.id("fb_age"));
+        List<WebElement> checkBoxes=driver.findElements(By.className("w3-check"));
+        List<WebElement> radioCircles=driver.findElements(By.className("w3-radio"));
+        Select opinionField= new Select(driver.findElement(By.id("like_us")));
+        WebElement submit=driver.findElement(By.cssSelector(".w3-btn-block"));
+        nameField.sendKeys("Chris");
+        ageField.sendKeys("18");
+        checkBoxes.get(1).click();
+        radioCircles.get(1).click();
+        opinionField.selectByVisibleText("Good");
+        submit.click();
+        Thread.sleep(1000);
+        WebElement name= driver.findElement(By.id("name"));
+        WebElement age= driver.findElement(By.id("age"));
+        WebElement languageField= driver.findElement(By.id("language"));
+        WebElement genderField= driver.findElement(By.id("gender"));
+        WebElement optionField= driver.findElement(By.id("option"));
+        WebElement commentField= driver.findElement(By.id("comment"));
+        assertEquals("Chris",name.getText());
+        assertEquals("18",age.getText());
+        assertEquals("French",languageField.getText());
+        assertEquals("female",genderField.getText());
+        assertEquals("Good",optionField.getText());
+        assertEquals("",commentField.getText());
+        WebElement yesButton=driver.findElement(By.cssSelector("button.w3-btn:nth-child(1)"));
+        WebElement noButton=driver.findElement(By.cssSelector("button.w3-btn:nth-child(2)"));
+        assertTrue("rgba(244, 67, 54, 1)".equals(noButton.getCssValue("background-color"))&&"rgba(255, 255, 255, 1)".equals(noButton.getCssValue("color")));
+        assertTrue("rgba(76, 175, 80, 1)".equals(yesButton.getCssValue("background-color"))&&"rgba(255, 255, 255, 1)".equals(yesButton.getCssValue("color")));
     }
 
     @Test
@@ -57,6 +128,24 @@ public class Task2 {
 //         click "Yes"
 //         check message text: "Thank you, NAME, for your feedback!"
 //         color of text is white with green on the background
+        WebElement nameField= driver.findElement(By.id("fb_name"));
+        nameField.sendKeys("Chris");
+        WebElement submit=driver.findElement(By.cssSelector(".w3-btn-block"));
+        submit.click();
+        Thread.sleep(1000);
+        WebElement yesButton=driver.findElement(By.cssSelector("button.w3-btn:nth-child(1)"));
+        yesButton.click();
+        Thread.sleep(1000);
+        WebElement message= driver.findElement(By.id("message"));
+        WebElement messageBox= driver.findElement(By.cssSelector(".w3-panel"));
+
+        assertEquals("Thank you, Chris, for your feedback!", message.getText());
+        assertTrue("rgba(76, 175, 80, 1)".equals(messageBox.getCssValue("background-color")));
+        assertTrue("rgba(255, 255, 255, 1)".equals(messageBox.getCssValue("color")));
+
+
+
+
     }
 
     @Test
@@ -66,6 +155,17 @@ public class Task2 {
 //         click "Yes"
 //         check message text: "Thank you for your feedback!"
 //         color of text is white with green on the background
+        WebElement submit=driver.findElement(By.cssSelector(".w3-btn-block"));
+        submit.click();
+        Thread.sleep(1000);
+        WebElement yesButton=driver.findElement(By.cssSelector("button.w3-btn:nth-child(1)"));
+        yesButton.click();
+        Thread.sleep(1000);
+        WebElement message= driver.findElement(By.id("message"));
+        WebElement messageBox= driver.findElement(By.cssSelector(".w3-panel"));
+        assertEquals("Thank you for your feedback!", message.getText());
+        assertTrue("rgba(76, 175, 80, 1)".equals(messageBox.getCssValue("background-color")));
+        assertTrue("rgba(255, 255, 255, 1)".equals(messageBox.getCssValue("color")));
     }
 
     @Test
@@ -75,5 +175,32 @@ public class Task2 {
 //         click "Send"
 //         click "No"
 //         check fields are filled correctly
+        WebElement nameField= driver.findElement(By.id("fb_name"));
+        WebElement ageField= driver.findElement(By.id("fb_age"));
+        List<WebElement> checkBoxes=driver.findElements(By.className("w3-check"));
+        List<WebElement> radioCircles=driver.findElements(By.className("w3-radio"));
+        Select opinionField= new Select(driver.findElement(By.id("like_us")));
+        WebElement submit=driver.findElement(By.cssSelector(".w3-btn-block"));
+        nameField.sendKeys("Chris");
+        ageField.sendKeys("18");
+        checkBoxes.get(1).click();
+        radioCircles.get(1).click();
+        opinionField.selectByVisibleText("Good");
+        submit.click();
+        Thread.sleep(1000);
+        WebElement noButton=driver.findElement(By.cssSelector("button.w3-btn:nth-child(2)"));
+        noButton.click();
+        Thread.sleep(1000);
+        nameField= driver.findElement(By.id("fb_name"));
+        ageField= driver.findElement(By.id("fb_age"));
+        checkBoxes=driver.findElements(By.className("w3-check"));
+        radioCircles=driver.findElements(By.className("w3-radio"));
+        opinionField= new Select(driver.findElement(By.id("like_us")));
+        assertEquals("Chris", nameField.getAttribute("value"));
+        assertEquals("18", ageField.getAttribute("value"));
+        assertTrue(radioCircles.get(1).isSelected());
+        assertTrue(checkBoxes.get(1).isSelected());
+        assertEquals(opinionField.getFirstSelectedOption().getText(), "Good");
+
     }
 }
